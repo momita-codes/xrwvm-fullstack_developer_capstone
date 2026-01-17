b038c073b712992f4e13eb85a85bea0573ad3e39
momita-codes <momitatudu28@gmail.com>
2026-01-18 00:50:47 +0530

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
       
     </Routes>    
   );
